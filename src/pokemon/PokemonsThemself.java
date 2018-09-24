f1f4f911a608ba9b74b9247b134f5fee33522ca6
Kleinau <Kleinau@ARLT3711-2.ads.uni-magdeburg.de>
2018-09-24 15:29:51 +0200

    Pokemons selbst hinzugefügt

---
+++ src/pokemon/PokemonsThemself.java
@@ -0,0 +1,10 @@
+package pokemon;
+
+public class PokemonsThemself {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, ich bin Pikatchu.");
+	}
+
+}
