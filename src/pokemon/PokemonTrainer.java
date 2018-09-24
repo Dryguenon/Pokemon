249c667728a56793d2c3b90b1cd10326d0797fc6
Dryguenon <samtpfote@kleinau.de>
2018-09-24 15:10:28 +0200

    Aenderungen an PokemonTrainer

--- src/pokemon/PokemonTrainer.java
+++ src/pokemon/PokemonTrainer.java
@@ -5,6 +5,7 @@ public class PokemonTrainer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, ich bin Ash.");
+		System.out.println("Ich bin ein Trainer.");
 	}
 
 }
