af256fb5e27d59397b794e7788d3bf2e7ce9c2ef
Kleinau <Kleinau@ARLT3711-2.ads.uni-magdeburg.de>
2018-09-24 14:48:28 +0200

    Ich habe das Java Projekt mit der PokemonTrainer Klasse erstellt.

---
+++ src/pokemon/PokemonTrainer.java
@@ -0,0 +1,10 @@
+package pokemon;
+
+public class PokemonTrainer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, ich bin Ash.");
+	}
+
+}
